fd188e6aff776cfc7f4e2106606be74aca833d9d
evgeniydukhanov <duhanoves@itwis.org>
2022-04-11 22:12:01 +0700

    installed parser

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const express = require("express");
 const { PORT = 3000 } = process.env;
 
 const app = express();
-app.listen(PORT);
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/mestodb", {
   useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
