7c2dc0ee65c2c91f60b21098134a56ff541cf2c7
evgeniydukhanov <duhanoves@itwis.org>
2022-04-24 19:09:38 +0700

    quotes fix

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const express = require("express");
+const express = require('express');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/mestodb", {
+mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 app.use((req, res, next) => {
   req.user = {
-    _id: "625592bc39d08a99d5deb442",
+    _id: '625592bc39d08a99d5deb442',
   };
 
   next();
 });
-app.use("/users", require("./routes/users"));
-app.use("/cards", require("./routes/cards"));
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
 
-app.all("*", (req, res) => {
-  res.status(404).send({ message: "По указанному пути ничего нет" });
+app.all('*', (req, res) => {
+  res.status(404).send({ message: 'По указанному пути ничего нет' });
 });
 
 app.listen(PORT, () => {
