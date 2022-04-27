a1fba16cd6ce8bacbf994286ccf2bd2b806a0106
evgeniydukhanov <duhanoves@itwis.org>
2022-04-27 21:05:29 +0700

    4 пункта вроде позади

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
-const express = require("express");
+const express = require('express');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/mestodb", {
+mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 app.use((req, res, next) => {
   req.user = {
-    _id: "625592bc39d08a99d5deb442",
+    _id: '625592bc39d08a99d5deb442',
   };
 
   next();
 });
-app.use("/users", require("./routes/users"));
-app.use("/cards", require("./routes/cards"));
 
-app.all("*", (req, res) => {
-  res.status(404).send({ message: "По указанному пути ничего нет" });
+const login = require('./controllers/users');
+const createUser = require('./controllers/users');
+
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
+
+app.post('/signin', login);
+app.post('/signup', createUser);
+
+app.all('*', (req, res) => {
+  res.status(404).send({ message: 'По указанному пути ничего нет' });
 });
 
 app.listen(PORT, () => {
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,12 @@
 const router = require('express').Router();
 
 const {
-  createUser,
   getUsers,
   getUser,
   patchAvatar,
   patchProfile,
 } = require('../controllers/users');
 
-router.post('/', createUser);
 router.get('/', getUsers);
 router.get('/:userId', getUser);
 router.patch('/me/avatar', patchAvatar);
