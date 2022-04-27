806041c58e01aecedd2af4caca3075a8d6ce0c53
evgeniydukhanov <duhanoves@itwis.org>
2022-04-27 21:24:18 +0700

    вроде 5ый пункт сделал

---
+++ middlewares/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const AuthorizationError = require('../errors/AuthorizationError');
+
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
+  if (!req.cookies.jwt) {
+    return res
+      .status(401)
+      .send({ message: 'Необходима авторизация' });
+  }
+
+  const token = req.cookies.jwt;
+  let payload;
+
+  try {
+    payload = jwt.verify(token, 'super-secret-key');
+  } catch (err) {
+    return new AuthorizationError('Необходима авторизация');
+  }
+
+  req.user = payload; // записываем пейлоуд в объект запроса
+
+  next(); // пропускаем запрос дальше
+};
