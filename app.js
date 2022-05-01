8a6bc609fc8e34ce4c301d5b269f80df92f011fc
evgeniydukhanov <duhanoves@itwis.org>
2022-05-01 19:35:23 +0700

    штрихи п.2

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
-// const { celebrate, Joi } = require('celebrate');
+
+const { celebrate, Joi, errors } = require('celebrate');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 app.use(bodyParser.json());
+app.use(cookieParser);
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const mongoose = require('mongoose');
@@ -19,18 +22,34 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const errorCatcher = require('./errors/errorCatcher');
+const NotFoundError = require('./errors/NotFoundError');
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/),
+  }),
+}), createUser)
 
 app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.all('*', (req, res) => {
-  res.status(404).send({ message: 'По указанному пути ничего нет' });
+app.all('*', (req, res, next) => {
+  next(NotFoundError('По указанному пути ничего нет'));
 });
 
+app.use(errors());
 app.use(errorCatcher);
 
 app.listen(PORT, () => {
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,9 +6,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    throw new AuthorizationError('Необходима авторизация');
   }
 
   const token = authorization.replace('Bearer ', '');
