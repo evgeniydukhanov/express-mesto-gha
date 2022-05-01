13e2e65cd8814751d264d29a0effc2f0cf46ff82
evgeniydukhanov <duhanoves@itwis.org>
2022-05-01 23:10:48 +0700

    валидация данных

--- routes/users.js
+++ routes/users.js
@@ -10,8 +10,28 @@ const {
 
 router.get('/', getUsers);
 router.get('/me', getMe);
-router.get('/:userId', getUser);
-router.patch('/me/avatar', patchAvatar);
-router.patch('/me', patchProfile);
+router.get('/:userId',
+  celebrate({
+    params: Joi.object().keys({
+      userId: Joi.string().length(24).hex().required(),
+    }),
+  }),
+  getUser,
+);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().pattern(reg).required(),
+  }),
+}),
+  patchAvatar,
+);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}),
+  patchProfile,
+);
 
 module.exports = router;
