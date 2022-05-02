78743783151d67a8faeb5375b06c97e32ab81279
evgeniydukhanov <duhanoves@itwis.org>
2022-05-02 15:15:56 +0700

    eslint fix

--- controllers/users.js
+++ controllers/users.js
@@ -9,17 +9,18 @@ const ValidationError = require('../errors/ValidationError');
 const ConflictError = require('../errors/ConflictError');
 
 module.exports.createUser = (req, res, next) => {
-  const { name, about, avatar, email, password } = req.body;
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
   bcrypt.hash(password, 10).then(
-    ((hash) =>
-      User.create({
-        email,
-        name,
-        about,
-        avatar,
-        password: hash,
-        select: false,
-      }))
+    ((hash) => User.create({
+      email,
+      name,
+      about,
+      avatar,
+      password: hash,
+      select: false,
+    }))
       .then((user) => res.send({ data: user }))
       .catch((err) => {
         if (err.name === 'ValidationError') {
