dea4b915c9c75b73938ec77b87ee95b38d0a01c6
evgeniydukhanov <duhanoves@itwis.org>
2022-05-04 07:09:22 +0700

    Revert "some changes"
    This reverts commit 227f59574e9d9c8fdaafb4160bedc5cf672035c7.

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
-// const { celebrate, Joi } = require('celebrate');
+
+const { celebrate, Joi, errors } = require('celebrate');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const mongoose = require('mongoose');
@@ -18,32 +21,36 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const errorCatcher = require('./errors/errorCatcher');
+const NotFoundError = require('./errors/NotFoundError');
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
 
-app.use(aut
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/),
+  }),
+}), createUser);
+
+app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.all('*', (req, res) => {
-  res.status(404).send({ message: 'По указанному пути ничего нет' });
+app.all('*', (req, res, next) => {
+  next(new NotFoundError('По указанному пути ничего нет'));
 });
 
-app.use((err, req, res, next) => {
-  // если у ошибки нет статуса, выставляем 500
-  const { statusCode = 500, message } = err;
-
-  res
-    .status(statusCode)
-    .send({
-      // проверяем статус и выставляем сообщение в зависимости от него
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка'
-        : message,
-    });
-  next();
-});
+app.use(errors());
+app.use(errorCatcher);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
