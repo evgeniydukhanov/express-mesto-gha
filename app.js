4fc5c72b80d4e4b536b11b4b83845664b863b6d4
evgeniydukhanov <duhanoves@itwis.org>
2022-05-08 16:50:21 +0700

    loggers & CORS

--- app.js
+++ app.js
@@ -23,6 +23,33 @@ const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const errorCatcher = require('./errors/errorCatcher');
 const NotFoundError = require('./errors/NotFoundError');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+];
+
+// eslint-disable-next-line prefer-arrow-callback
+app.use(function (req, res, next) {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  next();
+});
+
+app.use(requestLogger);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -48,6 +75,8 @@ app.use('/cards', require('./routes/cards'));
 app.all('*', (req, res, next) => {
   next(new NotFoundError('По указанному пути ничего нет'));
 });
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(errorCatcher);
