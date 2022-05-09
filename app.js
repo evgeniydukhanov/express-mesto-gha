92b0f633c9130653e705a3f1f44eae64bb584745
evgeniydukhanov <duhanoves@itwis.org>
2022-05-09 19:29:27 +0700

    console origin

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ const allowedCors = [
 // eslint-disable-next-line prefer-arrow-callback
 app.use(function (req, res, next) {
   const { origin } = req.headers;
+  console.log(origin);
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
