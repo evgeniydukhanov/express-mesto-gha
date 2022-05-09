c99bca76c47d24e1851fc68400305da429a2bdbe
evgeniydukhanov <duhanoves@itwis.org>
2022-05-09 19:31:42 +0700

    cors fixes

--- app.js
+++ app.js
@@ -50,9 +50,11 @@ app.use(function (req, res, next) {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  res.header('Access-Control-Allow-Headers', requestHeaders);
   if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    // res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
   return next();
