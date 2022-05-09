74054c1e73bbac637604ac29fb528b2ccd55785f
evgeniydukhanov <duhanoves@itwis.org>
2022-05-09 21:28:14 +0700

    authorization fight

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,13 +3,13 @@ const AuthorizationError = require('../errors/AuthorizationError');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const authorization = req.headers.jwt;
-  if (!authorization) {
+  const cookieAuthorization = req.cookies.jwt;
+  if (!cookieAuthorization) {
     return next(new AuthorizationError('Ошибка авторизации'));
   }
   let payload;
   try {
-    payload = jwt.verify(authorization, 'super-secret-key');
+    payload = jwt.verify(cookieAuthorization, 'super-secret-key');
   } catch (err) {
     return next(new AuthorizationError('Ошибка авторизации'));
   }
