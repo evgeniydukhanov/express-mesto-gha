d7b851b038f30fe0433135df0e9898e42d259ef9
evgeniydukhanov <duhanoves@itwis.org>
2022-05-09 18:47:17 +0700

    cors fixes

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.use(function (req, res, next) {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true);
   }
   const { method } = req;
