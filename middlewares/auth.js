85d8ca43f9005984a4249d978a8c4e957bb26647
evgeniydukhanov <duhanoves@itwis.org>
2022-05-09 21:39:23 +0700

    token ne rabotaet

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,14 +3,13 @@ const AuthorizationError = require('../errors/AuthorizationError');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer')) {
+  const cookieAuthorization = req.cookies.jwt;
+  if (!cookieAuthorization) {
     return next(new AuthorizationError('Ошибка авторизации'));
   }
-  const token = authorization.replace('Bearer', '');
   let payload;
   try {
-    payload = jwt.verify(token, 'super-secret-key');
+    payload = jwt.verify(cookieAuthorization, 'super-secret-key');
   } catch (err) {
     return next(new AuthorizationError('Ошибка авторизации'));
   }
