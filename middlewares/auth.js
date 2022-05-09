fd20098b02f7b6144fe9ff446a1b5b0f14424522
evgeniydukhanov <duhanoves@itwis.org>
2022-05-09 21:37:07 +0700

    token trying

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,13 +3,14 @@ const AuthorizationError = require('../errors/AuthorizationError');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const cookieAuthorization = req.cookies.jwt;
-  if (!cookieAuthorization) {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer')) {
     return next(new AuthorizationError('Ошибка авторизации'));
   }
+  const token = authorization.replace('Bearer', '');
   let payload;
   try {
-    payload = jwt.verify(cookieAuthorization, 'super-secret-key');
+    payload = jwt.verify(token, 'super-secret-key');
   } catch (err) {
     return next(new AuthorizationError('Ошибка авторизации'));
   }
