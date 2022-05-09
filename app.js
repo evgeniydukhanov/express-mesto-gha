8907aa798a463f65546b82a26016360fc9b37715
evgeniydukhanov <duhanoves@itwis.org>
2022-05-09 19:37:33 +0700

    CORS

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ app.use(function (req, res, next) {
   const { origin } = req.headers;
   console.log(origin);
   if (allowedCors.includes(origin)) {
+    console.log(123);
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
   }
@@ -51,6 +52,7 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
   res.header('Access-Control-Allow-Headers', requestHeaders);
   if (method === 'OPTIONS') {
+    console.log(321);
     // res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     // res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
